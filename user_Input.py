9df348cfab2cb829f2fccbec01021974139d1bf2
devyani001 <devyani140424@gmail.com>
2026-01-22 19:01:26 +0530

    Update user_Input.py

--- user_Input.py
+++ user_Input.py
@@ -1,12 +1,1 @@
-name = input()
-
-print('You entered : ', name)
-
-a = int(input('Enter a'))
-b = int(input('Enter b'))
-
-print(type(a))
-print(type(b))
-
-c = a * b
-print(c)
+print("Hello World!!!")
